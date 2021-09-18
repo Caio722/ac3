86d1207cdbaf1417dc4e3c0d406b9d3337a23ce2
Caio722 <caio.vinicius@aluno.faculdadeimpacta.com.br>
2021-09-18 09:23:07 -0300

    first commit

---
+++ ac3_cal.py
@@ -0,0 +1,48 @@
+
+class OperacaoFabrica(object):
+
+    def criar(self, operador):
+        if(operador == 'soma'):
+            return Soma()
+        elif(operador == 'subtracao'):
+            return Subtracao()
+        elif (operador == 'divisao'):
+            return Divisao()
+        elif (operador == 'multiplicacao'):
+            return Multiplicacao()
+
+
+class Operacao(metaclass=abc.ABCMeta):
+
+    @abc.abstractclassmethod
+    def executar(self, valor1, valor2):
+        pass
+
+
+class Soma(Operacao):
+    def executar(self, valor1, valor2):
+        resultado = valor1 + valor2
+        return resultado
+
+
+class Subtracao(Operacao):
+    def executar(self, valor1, valor2):
+        resultado = valor1 - valor2
+        return resultado
+
+
+class Divisao(Operacao):
+    def executar(self, valor1, valor2):
+        resultado = valor1 / valor2
+        return resultado
+
+
+class Multiplicacao(Operacao):
+    def executar(self, valor1, valor2):
+        resultado = valor1 * valor2
+        return resultado
+
+
+calculador = Calculadora()
+x = calculador.calcular(2, 3, 'soma')
+print(x)
