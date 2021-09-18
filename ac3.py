f97021b6996ccf7a1484fd22a198f1e9893a8b9a
Caio722 <caio.vinicius@aluno.faculdadeimpacta.com.br>
2021-09-18 09:29:28 -0300

    segundo commit

--- ac3.py
+++ ac3.py
@@ -1,32 +1,32 @@
 import unittest
-import Calculadora
+from CalculadoraBasica import CalculadoraBasica
+from CalculadorAvancada import CalculadorAvancada 
 
-
-class Teste(unittest.TestCase):
+class Testes(unittest.TestCase):
     def test_soma(self):
-        calculador = Calculadora.Calculadora()
+        calculador = Calculadora()
         result = calculador.calcular(2, 3, 'soma')
         self.assertEqual(result, 5)
 
     def test_multiplicacao(self):
-        calculador = Calculadora.Calculadora()
+        calculador = Calculadora()
         result = calculador.calcular(2, 5, 'multiplicacao')
         self.assertEqual(result, 10)
 
     def test_Divisao(self):
-        calculador = Calculadora.Calculadora()
+        calculador = Calculadora()
         result = calculador.calcular(2, 4, 'divisao')
         self.assertEqual(result, 0.5)
 
     def test_subtracao(self):
-        calculador = Calculadora.Calculadora()
+        calculador = Calculadora()
         result = calculador.calcular(2, 4, 'subtracao')
         self.assertEqual(result, -2)
 
     def test_subtracao2(self):
-        calculador = Calculadora.Calculadora()
-        result = calculador.calcular(2, 4, 'subtrair')
-        self.assertEqual(result, 0)
+        calculador = Calculadora()
+        self.assertEqual (calculador.calcular(2, 4, 'subtrair'), 0)
+        
 
 
 if __name__ == '__main__':
